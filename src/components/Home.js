6d69dec3bbf85088dcc7779b323d8c1212becd08
Irem Peker <irem.peker@gmx.de>
2020-08-14 20:04:57 +0200

    Added instructions in the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,13 @@ const Home = () => {
         <section>
             <div className="how-to">
                 <h1>Quiz Game</h1>
-                <ul>Here are the instructions
-                    <li>Duration of the game is 15 minutes</li>
-                    <li>Each game has 15 questions</li>
-                    <li>Each question has 3 options
+                <div className="instructions">
+                    <p>Duration of the game is 15 minutes</p>
+                    <p>Each game has 15 questions</p>
+                    <p>Each question has 3 options
                         <img src={spontago} class="example-screenshot" alt="example screenshot"></img>
-                    </li>
-                </ul>
+                    </p>
+                </div>
                 <div className="play-button-container">
                     <button className="play-button"><Link to="play">Play</Link></button>
                 </div>
