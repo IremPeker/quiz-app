be9b2d2f2d0134cbf03073ec490a3f0237873f33
Irem Peker <irem.peker@gmx.de>
2020-08-14 12:06:28 +0200

    Deleted unnecessary files and added react router

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+      <div>
+        <p className="home">
+          Welcome Home
+        </p>
+      </div>
+    );
+  };
+  
+  export default Home;
