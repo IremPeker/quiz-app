09a64d1d105df3b3ac086b78552047ea3f38aacf
Irem Peker <irem.peker@gmx.de>
2020-08-16 16:04:59 +0200

    Added styling and fetched the data

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,52 @@ import HomeContainer from './HomeContainer';
 import PlayContainer from './PlayContainer';
 import '../styles/App.scss';
 
-export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/"> <HomeContainer /> </Route>
-        <Route path="/play"> <PlayContainer /> </Route>
-      </Switch>
-    </Router>
-  );
+class App extends React.Component  {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+      urlError: false
+    };
+
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  async fetchData() {
+    let url = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple';
+    
+    try {
+      const response = await fetch(url, {
+        method: "get"
+      });
+      const allQuestions = await response.json();
+      const seeQuestions = allQuestions.results;
+
+      console.log("questions are", allQuestions);
+      
+
+    } catch (error) {
+      this.setState({
+        urlError: true
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/"> <HomeContainer /> </Route>
+          <Route path="/play"> <PlayContainer randomPhotos={this.state.randomPhotos}></PlayContainer></Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
+export default App;
