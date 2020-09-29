774f5c6d60d2d0910644cb4f93a4e0b6b47a9515
Irem Peker <irem.peker@gmx.de>
2020-09-29 12:07:51 +0200

    Updates

---
+++ precache-manifest.1dd8f43b994771e7b19e0fc13e6f4071.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7f5a14c5a2dceba9e9fb3a69ae35c9",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "56d00bef8664228f0099",
+    "url": "/quiz-app/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "6d35f950f3a043d54ae5",
+    "url": "/quiz-app/static/css/main.c5e3a7df.chunk.css"
+  },
+  {
+    "revision": "56d00bef8664228f0099",
+    "url": "/quiz-app/static/js/2.ed019d29.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/quiz-app/static/js/2.ed019d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d35f950f3a043d54ae5",
+    "url": "/quiz-app/static/js/main.a5e9c14c.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  },
+  {
+    "revision": "e3c65c503858e9c3decc18f476bf0b06",
+    "url": "/quiz-app/static/media/fail.e3c65c50.webp"
+  },
+  {
+    "revision": "30eac6cc727aa3baac2742e1c394703c",
+    "url": "/quiz-app/static/media/game.30eac6cc.png"
+  },
+  {
+    "revision": "4273b51217106ee11e0c2e4dc665a3d8",
+    "url": "/quiz-app/static/media/olav-ahrens-rotne-jvBXiynINGE-unsplash.4273b512.jpg"
+  }
+]);
